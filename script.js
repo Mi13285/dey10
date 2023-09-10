3fbb5fc86beb4b2f85dcfd1b3f3ede824f18f495
Mi13285 <mixkud95@inbox.ru>
2023-09-10 10:16:41 +0300

    Старт

---
+++ script.js
@@ -0,0 +1,11 @@
+function Showmovie()){
+    let age=prompt("Ваш возраст?")
+if(age>=18)
+    alert("доступ Разрешен")
+if(age<18)
+ confirm("Родители разрешили?")
+alert("Доступ закрыт")
+}
+Showmovie()
+
+
